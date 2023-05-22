86fdbc2ce1d142c54a9cbbf83087a470f9bee3b1
LIYU <liyu0x@outlook.com>
2023-05-22 18:09:31 +0800

    upload

--- katan_64_bct_search.py
+++ katan_64_bct_search.py
@@ -4,13 +4,9 @@ from katan_64_bct_search_task import find_single_trail
 
 MAX_THREAD = 12
 
-MAX_SINGLE_TRAIL_SERACH_LIMIT = 4
-MAX_CLUSTER_TRAIL_SERACH_LIMIT = 4
-SWITCH_ROUNDS = 4
-WORDSIZE = 32
+SWITCH_ROUNDS = 7
 
-POOL = multiprocessing.Pool(processes=MAX_THREAD)
-
+POOL = multiprocessing.get_context("fork").Pool(MAX_THREAD)
 
 if __name__ == '__main__':
     c = katan64_bct.katan64()
@@ -20,7 +16,8 @@ if __name__ == '__main__':
         task_list = []
         for _ in range(MAX_THREAD):
             task_list.append(
-                POOL.apply_async(find_single_trail, args=(c, start_rounds, 0, int(start_rounds / 2), SWITCH_ROUNDS,20)))
+                POOL.apply_async(find_single_trail,
+                                 args=(c, start_rounds, 0, int(start_rounds / 2), SWITCH_ROUNDS, 33)))
             start_rounds += 1
             if start_rounds > end_ends:
                 break

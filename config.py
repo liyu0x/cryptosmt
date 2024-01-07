c5c77fd1573ee5a5473f1c3d8dfb798dd9f2e99c
LIYU <liyu0x@outlook.com>
2024-01-07 21:55:46 +0800

    upload

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 # Paths to the STP and cryptominisat executable
-#PATH_STP = "../stp/build/stp"
-PATH_STP = "/opt/homebrew/bin/stp"
-#PATH_STP = "./windows_x86/stp-win64.exe"
-#PATH_CRYPTOMINISAT = "./windows_x86/cryptominisat5.exe"
-PATH_CRYPTOMINISAT = "/opt/homebrew/bin/cryptominisat5"
+PATH_STP = "../stp/build/stp"
+PATH_CRYPTOMINISAT = "../cryptominisat/build/cryptominisat5"
+PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
 PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
 # Maximum weight for characteristics to search for
 MAX_WEIGHT = 1000
@@ -11,5 +9,3 @@ MAX_WEIGHT = 1000
 MAX_CHARACTERISTICS = 10000000
 MULTI_THREADS = 6
 USE_SHARP = 1
-PATH_SHARP = "./sharpSAT"
-

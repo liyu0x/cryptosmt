7dcb4e41d11205e195812b956e6e7f8699e740c5
0xliyu <liyu0x@outlook.com>
2024-03-17 14:51:29 +0800

    upload

--- ciphers/sand_sbox.py
+++ ciphers/sand_sbox.py
@@ -2,6 +2,7 @@ from ciphers.cipher import AbstractCipher
 from parser import stpcommands
 import util
 import itertools
+import numpy as np
 
 
 class Sand(AbstractCipher):
@@ -53,29 +54,27 @@ class Sand(AbstractCipher):
             # self.pre_round(stp_file, xl[0], xr[0], xl[1], xr[1], block_size)
 
             for i in range(e0_start_search_num, e0_end_search_num):
-                self.setup_round(stp_file, variables["in_left"][i], variables["in_right"][i],
-                                 variables["in_left"][i + 1], variables["in_right"][i + 1],
+                self.setup_round(stp_file, variables["xl"][i], variables["xr"][i],
+                                 variables["xl"][i + 1], variables["xr"][i + 1],
                                  variables["g0_rot"][i], variables["g0_box_out"][i],
                                  variables["g1_rot"][i], variables["g1_box_out"][i],
                                  variables["g01_xor_out"][i], variables["perm_out"][i], variables["w"][i], block_size)
             # BCT
-            # for i in range(em_start_search_num, em_end_search_num):
-            #     self.bct_operator(stp_file, xl[i], yr[i + 1], g0_rot[i]
-            #                       , g0_xor_out[i], g1_rot[i],
-            #                       g1_xor_out[i], g12_xor_out[i], perm_out[i],
-            #                       w[i], and_out[i], and_f[i], block_size)
+            for i in range(em_start_search_num, em_end_search_num):
+                self.bct_operator(stp_file, variables["xl"][i], variables["yr"][i + 1], variables["w"][i])
 
-            # for i in range(e1_start_search_num, e1_end_search_num):
-            #     self.setup_round(stp_file, yl[i], yr[i], yl[i + 1], yr[i + 1], g0_rot[i]
-            #                      , g0_xor_out[i], g1_rot[i],
-            #                      g1_xor_out[i], g12_xor_out[i], perm_out[i],
-            #                      w[i], and_out[i], and_f[i], block_size)
+            for i in range(e1_start_search_num, e1_end_search_num):
+                self.setup_round(stp_file, variables["yl"][i], variables["yr"][i],
+                                 variables["yl"][i + 1], variables["yr"][i + 1],
+                                 variables["g0_rot"][i], variables["g0_box_out"][i],
+                                 variables["g1_rot"][i], variables["g1_box_out"][i],
+                                 variables["g01_xor_out"][i], variables["perm_out"][i], variables["w"][i], block_size)
 
             command += self.pre_handle(parameters)
             stp_file.write(command)
-            stpcommands.assertNonZero(stp_file, [variables["in_left"][0], variables["in_right"][0]], block_size)
+            stpcommands.assertNonZero(stp_file, [variables["xl"][0], variables["xr"][0]], block_size)
             if switch_rounds > 0:
-                stpcommands.assertNonZero(stp_file, [variables["in_left"][rounds], variables["in_right"][rounds]],
+                stpcommands.assertNonZero(stp_file, [variables["yl"][rounds], variables["yr"][rounds]],
                                           block_size)
 
             for key, value in parameters["fixedVariables"].items():
@@ -90,53 +89,64 @@ class Sand(AbstractCipher):
             command += "ASSERT({0}[{1}:{1}]={2}[{3}:{3}]);\n".format(out_right, i, in_right, re_sharp[i])
         stp_file.write(command)
 
-    def bct_operator(self, stp_file, xl, yr, g0_rot
-                     , g0_xor_out, g1_rot,
-                     g1_xor_out, g12_xor_out, perm_out,
-                     w, and_out, and_f, block_size):
+    def bct_operator(self, stp_file, in_left, out_right, w):
 
-        nibble = block_size // 4
-        x0_index = util.sand_rot_nibble(block_size, self.alpha)
+        bct = create_bct(self.G0_BOX, self.G1_BOX)
+
+        trails = []
+
+        # All zero trail with probability 1
+        for input_diff in range(16):
+            for output_diff in range(16):
+                if bct[input_diff][output_diff] != 0:
+                    tmp = []
+                    tmp.append((input_diff >> 3) & 1)
+                    tmp.append((input_diff >> 2) & 1)
+                    tmp.append((input_diff >> 1) & 1)
+                    tmp.append((input_diff >> 0) & 1)
+                    tmp.append((output_diff >> 3) & 1)
+                    tmp.append((output_diff >> 2) & 1)
+                    tmp.append((output_diff >> 1) & 1)
+                    tmp.append((output_diff >> 0) & 1)
+                    if bct[input_diff][output_diff] == 2:
+                        tmp += [0, 1, 1, 1]  # 2^-3
+                    elif bct[input_diff][output_diff] == 4:
+                        tmp += [0, 0, 1, 1]  # 2^-2
+                    elif bct[input_diff][output_diff] == 8:
+                        tmp += [0, 0, 0, 1]  # 2^-1
+                    elif bct[input_diff][output_diff] == 16:
+                        tmp += [0, 0, 0, 0]
+                    trails.append(tmp)
+
+        variables_list = []
+        for i in range(8):
+            variables = ["{0}[{1}:{1}]".format(in_left, 24 + i),
+                         "{0}[{1}:{1}]".format(in_left, 16 + i),
+                         "{0}[{1}:{1}]".format(in_left, 8 + i),
+                         "{0}[{1}:{1}]".format(in_left, i),
+                         "{0}[{1}:{1}]".format(out_right, 24 + i),
+                         "{0}[{1}:{1}]".format(out_right, 16 + i),
+                         "{0}[{1}:{1}]".format(out_right, 8 + i),
+                         "{0}[{1}:{1}]".format(out_right, i),
+                         "{0}[{1}:{1}]".format(w, 24 + i),
+                         "{0}[{1}:{1}]".format(w, 16 + i),
+                         "{0}[{1}:{1}]".format(w, 8 + 1),
+                         "{0}[{1}:{1}]".format(w, i)]
+            variables_list.append(variables)
+
         command = ""
-        # in G0   x{3} AND x{2}
-        x0_3 = x0_index[3]
-        x0_2 = x0_index[2]
+        for variables in variables_list:
+            cnf = ""
+            for prod in itertools.product([0, 1], repeat=len(trails[0])):
+                # Trail is not valid
+                if list(prod) not in trails:
+                    expr = ["~" if x == 1 else "" for x in list(prod)]
+                    clause = ""
+                    for literal in range(12):
+                        clause += "{0}{1} | ".format(expr[literal], variables[literal])
 
-        for i in range(nibble):
-            command += "ASSERT({0}[{2}:{2}]&{1}[{3}:{3}]={1}[{2}:{2}]&{0}[{3}:{3}]);\n".format(
-                xl, yr, x0_3[i], x0_2[i]
-            )
-
-        #  (x{3} AND x{2} XOR x{0}) AND x{1}
-        x0_0 = x0_index[0]
-        x0_1 = x0_index[1]
-
-        for i in range(nibble):
-            command += ("ASSERT(BVXOR({0}[{2}:{2}]&{0}[{3}:{3}], {0}[{4}:{4}])&{1}[{5}:{5}]="
-                        "BVXOR({1}[{2}:{2}]&{1}[{3}:{3}], {1}[{4}:{4}])&{0}[{5}:{5}]);\n").format(
-                xl, yr, x0_3[i], x0_2[i], x0_0[i], x0_1[i],
-
-            )
-
-        # in G1   x{1} AND x{3}
-        x1_index = util.sand_rot_nibble(block_size, self.beta)
-        x1_3 = x1_index[3]
-        x1_1 = x1_index[1]
-
-        for i in range(nibble):
-            command += "ASSERT({0}[{2}:{2}]&{1}[{3}:{3}]={1}[{2}:{2}]&{0}[{3}:{3}]);\n".format(
-                xl, yr, x1_3[i], x1_1[i])
-
-        x1_2 = x1_index[2]
-        x1_0 = x1_index[0]
-
-        #  (x{3} AND x{1} XOR x{2}) AND x{0}
-        for i in range(nibble):
-            command += ("ASSERT(BVXOR({0}[{2}:{2}]&{0}[{3}:{3}], {0}[{4}:{4}])&{1}[{5}:{5}]="
-                        "BVXOR({1}[{2}:{2}]&{1}[{3}:{3}], {1}[{4}:{4}])&{0}[{5}:{5}]);\n").format(
-                xl, yr, x1_3[i], x1_1[i], x1_2[i], x1_0[i]
-
-            )
+                    cnf += "({}) &".format(clause[:-2])
+            command += "ASSERT({} = 0bin1);\n".format(cnf[:-2])
 
         stp_file.write(command)
 
@@ -306,7 +316,7 @@ def initial_file(rounds, block_size, weight, stp_file):
     stpcommands.setupVariables(stp_file, w, block_size * 2)
     stpcommands.setupWeightComputation(stp_file, weight, w, block_size * 2)
     # setupWeightComputationSum(stp_file, weight, w, block_size)
-    return {"in_left": xl, "in_right": xr, "out_left": yl, "out_right": yr, "g0_rot": g0_rot, "g0_box_out": g0_box_out,
+    return {"xl": xl, "xr": xr, "yl": yl, "yr": yr, "g0_rot": g0_rot, "g0_box_out": g0_box_out,
             "g1_rot": g1_rot, "g1_box_out": g1_box_out, "perm_out": perm_out, "w": w, "g01_xor_out": g01_xor_out}
 
 
@@ -425,3 +435,16 @@ def add4bitSbox(s_box_trails, left_in, s_out, w, indexes):
             cnf += "({}) &".format(clause[:-2])
 
     return "ASSERT({} = 0bin1);\n".format(cnf[:-2])
+
+
+def create_bct(g0_s_box, g1_s_box):
+    bct = np.zeros((2 ** 4, 2 ** 4), dtype=np.int8)
+    for x1 in range(2 ** 4):
+        for delta_in in range(2 ** 4):
+            for delta_out in range(2 ** 4):
+                y = g0_s_box[x1] ^ g1_s_box[x1] ^ g0_s_box[x1 ^ delta_in] ^ g1_s_box[x1 ^ delta_in] ^ g0_s_box[
+                    x1 ^ delta_out] ^ g1_s_box[x1 ^ delta_out] ^ g0_s_box[x1 ^ delta_in ^ delta_out] ^ g1_s_box[
+                        x1 ^ delta_in ^ delta_out]
+                if y == 0:
+                    bct[delta_in][delta_out] += 1
+    return bct

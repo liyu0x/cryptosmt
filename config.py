bcbc16145e133ccb9c18e07184ea7effd3f2c287
LY <liyu0x@outlook.com>
2024-05-22 12:31:06 +0800

    upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Paths to the STP and cryptominisat executable
 
 # LINUX
-# PATH_STP = "../stp/build/stp"
-# PATH_CRYPTOMINISAT = "../cryptominisat/build/cryptominisat5"
-# PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
-# PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
+PATH_STP = "/home/linuxbrew/.linuxbrew/bin/stp"
+PATH_CRYPTOMINISAT = "/home/linuxbrew/.linuxbrew/bin/cryptominisat5"
+PATH_SHARP = "./sharpSAT"
+PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
 
 # MACOS
 # PATH_STP = "/opt/homebrew/bin/stp"
@@ -13,14 +13,14 @@
 # PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
 
 # Windows
-PATH_STP = "../outer/stp"
-PATH_CRYPTOMINISAT = "../outer/cryptominisat5"
-PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
-PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
+# PATH_STP = "../outer/stp"
+# PATH_CRYPTOMINISAT = "../outer/cryptominisat5"
+# PATH_SHARP = "../sharpsat-td/bin/sharpSAT"
+# PATH_BOOLECTOR = "/opt/homebrew/bin/boolector"
 
 # Maximum weight for characteristics to search for
 MAX_WEIGHT = 1000
 # Maximum number of characteristics to search for a differential
 MAX_CHARACTERISTICS = 10000000
-MULTI_THREADS = 6
-USE_SHARP = 0
+MULTI_THREADS = 10
+USE_SHARP = 1
